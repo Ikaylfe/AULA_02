0669ab43c5150931965192f34bc2655c7fa1b725
Ikaylfe <kayane.brito.7@gmail.com>
2026-04-02 21:36:48 -0300

    inicia atividade 03

---
+++ atividade03.py
@@ -0,0 +1,11 @@
+# ----- criar média aluno
+
+aluno='Bruno'
+nota_01=90
+nota_02=70
+nota_03=80
+nota_04=90
+
+media=(nota_01+nota_02+nota_03+nota_04)/4
+
+print (f'Média anual do {aluno} é = {media}')
